385f60e2dca4ac776716ac5d67184b2b36309343
muyezu <42454173+muyezu@users.noreply.github.com>
2019-04-11 14:59:39 -1000

    Update barrier.js

--- barrier.js
+++ barrier.js
@@ -1,19 +1,31 @@
 function barrier() {
-this.velocity = 10;
-this.height = 100;
-this.width = 300;
-this.a = 1356 + this.width; //X AXIS
-this.b = Math.floor((Math.random() * h) - this.width); //Y AXIS
-this.show = function() {
-fill(color('blue'));    
-rect(this.a, this.b, this.height, this.width);
-this.a -= this.velocity;
-}
-this.update = function() {
-  if(this.a < 0 - 300) {
-  this.velocity === 0;
-  this.b === Math.floor((Math.random() * h) - this.width); //Y AXIS
-  this.a === 1356 + this.width; //X AXIS
+  this.velocity = 1;
+  this.height = 100;
+  this.width = 300;
+  this.a = 1356 + this.width; //X AXIS
+  this.b = Math.floor((Math.random() * h) - this.width); //Y AXIS
+
+  this.show = function() {
+  fill(color('blue'));    
+  rect(this.a, this.b, this.height, this.width);
+  if (this.a > 0) {
+  this.velocity ++;
   }
+  else {}
+
+  if (this.a < 0) {
+    this.velocity = 0;
+    this.a = 1356 + this.width;
+    this.b = Math.floor((Math.random() * h) - this.width); 
+    score += 1;
   }
-}
+    this.a -= this.velocity;
+  }
+  this.update = function() {
+    if(this.a < 0) {
+    this.velocity === 0;
+    this.b === Math.floor((Math.random() * h) - this.width); //Y AXIS
+    this.a === 1300 + this.width; //X AXIS
+    }
+    }
+  }
