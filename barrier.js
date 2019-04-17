004d6ed1a2601d1b06de186c75ed9e4526a8df6f
muyezu <42454173+muyezu@users.noreply.github.com>
2019-04-16 14:56:24 -1000

    Update barrier.js

--- barrier.js
+++ barrier.js
@@ -1,5 +1,5 @@
 function barrier() {
-  this.velocity = 1;
+  this.velocityB = 1;
   this.height = 100;
   this.width = 300;
   this.a = 1356 + this.width; //X AXIS
@@ -9,21 +9,21 @@ function barrier() {
   fill(color('blue'));    
   rect(this.a, this.b, this.height, this.width);
   if (this.a > 0) {
-  this.velocity ++;
+  this.velocityB ++;
   }
   else {}
 
   if (this.a < 0) {
-    this.velocity = 0;
-    this.a = 1356 + this.width;
+    this.velocityB = 0;
+    this.a = 1366 + this.width;
     this.b = Math.floor((Math.random() * h) - this.width); 
     score += 1;
   }
-    this.a -= this.velocity;
+    this.a -= this.velocityB;
   }
   this.update = function() {
     if(this.a < 0) {
-    this.velocity === 0;
+    this.velocityB === 0;
     this.b === Math.floor((Math.random() * h) - this.width); //Y AXIS
     this.a === 1300 + this.width; //X AXIS
     }
